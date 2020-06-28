b2537651d4d56f940a9e836e15093a73b771032f
Leandro Dos Anjos <laamaker@gmail.com>
2020-06-28 15:20:28 -0300

    Added concrete repositories

---
+++ src/Lazulisoft.ApiCleanArchtMediatR.Infrastructure/Repositories/CharacterRepository.cs
@@ -0,0 +1,19 @@
+﻿using Lazulisoft.ApiCleanArchtMediatR.Domain.Entities;
+using Lazulisoft.ApiCleanArchtMediatR.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace Lazulisoft.ApiCleanArchtMediatR.Infrastructure.Repositories
+{
+    public class CharacterRepository : GenericRepository<Character>, ICharacterRepository
+    {
+        public CharacterRepository(DbContext dbContext) : base(dbContext)
+        {
+        }
+
+        public virtual async Task<Character> GetById(int id)
+        {
+            return await base.Find(x => x.Id == id);
+        }
+    }
+}
---
+++ src/Lazulisoft.ApiCleanArchtMediatR.Infrastructure/Repositories/GenericRepository.cs
@@ -0,0 +1,58 @@
+﻿using Lazulisoft.ApiCleanArchtMediatR.Domain.Entities;
+using Lazulisoft.ApiCleanArchtMediatR.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Lazulisoft.ApiCleanArchtMediatR.Infrastructure.Repositories
+{
+    public class GenericRepository<TEntity> : IRepository<TEntity> where TEntity : BaseEntity
+    {
+        private readonly DbContext DbContext;
+
+        public GenericRepository(DbContext dbContext)
+        {
+            DbContext = dbContext;
+        }
+
+        public virtual async Task<IEnumerable<TEntity>> GetAll()
+        {
+            return await DbContext.Set<TEntity>().ToListAsync();
+        }
+
+        public virtual IQueryable<TEntity> All()
+        {
+            return DbContext.Set<TEntity>().AsQueryable();
+        }
+
+        public virtual async Task<TEntity> Find(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await DbContext.Set<TEntity>().FirstOrDefaultAsync(predicate);
+        }
+
+        public virtual bool Contains(Expression<Func<TEntity, bool>> predicate)
+        {
+            return DbContext.Set<TEntity>().Count(predicate) > 0;
+        }
+
+        public virtual void Add(TEntity entity)
+        {
+            DbContext.Set<TEntity>().Add(entity);
+        }
+
+        public virtual void Update(TEntity entity)
+        {
+            var entry = DbContext.Entry(entity);
+            DbContext.Set<TEntity>().Attach(entity);
+            entry.State = EntityState.Modified;
+        }
+
+        public virtual void Delete(TEntity entity)
+        {
+            DbContext.Set<TEntity>().Remove(entity);
+        }
+    }
+}
