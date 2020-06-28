0382d664241c041d4bb08d597d5f671c1eefd045
Leandro Dos Anjos <laamaker@gmail.com>
2020-06-28 16:37:41 -0300

    Added DependencyInjection class to Infrastructure project

---
+++ src/Lazulisoft.ApiCleanArchtMediatR.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Lazulisoft.ApiCleanArchtMediatR.Domain.Data;
+using Lazulisoft.ApiCleanArchtMediatR.Domain.Repositories;
+using Lazulisoft.ApiCleanArchtMediatR.Infrastructure.Data;
+using Lazulisoft.ApiCleanArchtMediatR.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Lazulisoft.ApiCleanArchtMediatR.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<DbContext, ApplicationDbContext>();
+            services.AddTransient<ICharacterRepository, CharacterRepository>();
+            services.AddTransient<IUnitOfWork, UnitOfWork>();
+            return services;
+        }
+    }
+}
