355530d8b169929711477fe667e5bd310dba4722
Leandro Dos Anjos <laamaker@gmail.com>
2020-06-28 17:44:36 -0300

    Added CharacterDto class to Application project

---
+++ src/Lazulisoft.ApiCleanArchtMediatR.Application/Characters/Dtos/CharacterDto.cs
@@ -0,0 +1,23 @@
+﻿using Lazulisoft.ApiCleanArchtMediatR.Domain.Enums;
+
+namespace Lazulisoft.ApiCleanArchtMediatR.Application.Characters.Dtos
+{
+    public class CharacterDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string FullName { get; set; }
+
+        public string Description { get; set; }
+
+        public string Homeworld { get; set; }
+
+        public string Species { get; set; }
+
+        public Gender Gender { get; set; }
+
+        public string Occupation { get; set; }
+    }
+}
